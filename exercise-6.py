8a652df1cd326532185722cfe7043095b3e8e62e
Andrea Ranzato <andrea.ranzato01@gmail.com>
2022-01-09 16:02:00 +0100

    Add Apache read log file in exercise-6.py

--- exercise-6.py
+++ exercise-6.py
@@ -1,3 +1,9 @@
+def pig_latin(word):
+    '''Translate english words to pig latin (solution)'''
+    if word[0] in 'aeiou':
+        return f'{word}way'
+    return f'{word[1:]}{word[0]}ay'
+
 def pl_sentence(a_sentence):
     '''Translate into pig latin more than one word'''
     input = a_sentence.split()
@@ -22,31 +28,21 @@ def nonsensical_sentence(file_path):
     output = ''.join(f'{word}{" "}' for word in nosense)
     return output.strip()
 
-def word_per_line(filename):
-    """Given a text file, return a sentence from the nth
-    word for line n, for each of the first 10 lines. (solution)
-    """
-    output = []
-
-    for n, one_line in enumerate(open(filename)):
-        words = one_line.split()
-
-        if not words:
-            continue
-
-        if n >= 10:
-            break
-
-        if n >= len(words):
-            output.append(words[-1])
-        else:
-            output.append(words[n])
-
-    return ' '.join(output)
+# apache log file error example:
+# 127.0.0.1 - - [28/Jul/2006:10:27:32 -0300] "GET /hidden/ HTTP/1.0" 404 7218
+def find_ip_404_error(log_file_name):
+    '''Find ip of a 404 error in apache log file'''
+    with open(log_file_name) as lf:
+        for line in lf:
+            if ' 404 ' in line:
+                ip = line.split()[0]
+    return ip
 
 if __name__ == "__main__":
     test = 'this is a test translation'
     print(pl_sentence(test))
     print(nonsensical_sentence('data/exercise-6-data.txt'))
-    print(word_per_line('data/exercise-6-data.txt'))
+    print(find_ip_404_error('data/apache-log-file.txt'))
+
+
 
