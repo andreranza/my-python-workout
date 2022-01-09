31373d0cba9a065316ca7e7e56f4be95a9ca4035
Andrea Ranzato <andrea.ranzato01@gmail.com>
2022-01-09 15:36:29 +0100

    Remove translation from pig latin

--- exercise-6.py
+++ exercise-6.py
@@ -1,9 +1,3 @@
-def pig_latin(word):
-    '''Translate english words to pig latin (solution)'''
-    if word[0] in 'aeiou':
-        return f'{word}way'
-    return f'{word[1:]}{word[0]}ay'
-
 def pl_sentence(a_sentence):
     '''Translate into pig latin more than one word'''
     input = a_sentence.split()
@@ -11,6 +5,9 @@ def pl_sentence(a_sentence):
     return output
 
 def nonsensical_sentence(file_path):
+    """Given a text file, return a sentence from the nth
+    word for line n, for each of the first 10 lines. 
+    """
     nosense = list()
     with open(file_path) as f:
         for i, line in enumerate(f):
@@ -18,14 +15,38 @@ def nonsensical_sentence(file_path):
                 stripped_line = line.strip()
                 for j, word in enumerate(stripped_line.split()):
                     if j == i:
-                        nosense.append(pig_latin(word))
+                        nosense.append(word)
             else:
                 break
+
     output = ''.join(f'{word}{" "}' for word in nosense)
     return output.strip()
 
+def word_per_line(filename):
+    """Given a text file, return a sentence from the nth
+    word for line n, for each of the first 10 lines. (solution)
+    """
+    output = []
+
+    for n, one_line in enumerate(open(filename)):
+        words = one_line.split()
+
+        if not words:
+            continue
+
+        if n >= 10:
+            break
+
+        if n >= len(words):
+            output.append(words[-1])
+        else:
+            output.append(words[n])
+
+    return ' '.join(output)
+
 if __name__ == "__main__":
     test = 'this is a test translation'
-    #print(pl_sentence(test))
+    print(pl_sentence(test))
     print(nonsensical_sentence('data/exercise-6-data.txt'))
+    print(word_per_line('data/exercise-6-data.txt'))
 
